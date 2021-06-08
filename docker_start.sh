7ebfa624c1dcb9cb8f5bac31baf7aa5cb8dda9c5
LuckyLuke147 <mwojciuk@griddynamics.com>
2021-06-08 22:23:24 +0200

    docker images

---
+++ docker_start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# docker login --username zpi-twitter --password Qwerty1234 zpitwitteranalysis.jfrog.io
+docker login --username marwoj --password Qwerty1234 zpitwitteranalysis.jfrog.io
+docker-compose up -d
